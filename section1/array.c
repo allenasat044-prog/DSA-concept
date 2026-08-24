40f70a6165cbb6de0e9bb5e664e0596f9766e4d8
allenasat044-prog <allenasat044@gmail.com>
2026-08-24 18:43:30 +0530

    adding way to initialize array

---
+++ section1/array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int A[5]; // initialization of an array
+    int B[5]={2,4,6,8,10}; // declaration of an array
+    int i;
+    for(int i=0;i<5;i++)
+    {
+        printf("%d",B[i]);
+    }
+}
---
+++ section1/array.cpp
@@ -0,0 +1,36 @@
+// Ways in which we can initialize an array
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // First way
+    int A[] = {1, 2, 3, 4};
+    cout << A[2] << endl;
+
+    // Second way
+    int B[5];
+    B[0] = 2;
+    B[1] = 3;
+
+    cout << sizeof(B) << endl;
+    cout << B[0] << endl;
+    cout << B[1] << endl;
+
+    // Third way
+    const int n = 5;
+    int C[n] = {};
+    C[1] = 23;
+    for (int x : C) {
+        cout << x << endl;  //for each loop
+    }
+
+    // Fourth way
+    int D[5] = {2, 6, 8, 10};
+    for (int i = 0; i < 5; i++) {
+        cout << D[i] << endl;
+    }
+
+    return 0;
+}
